d38ffc3183fe3d0413135fccd312ffbe99328d12
Михаил Вагин <mihail.vaginkpc592020@gmail.com>
2024-05-09 00:19:39 +0300

    Fix bug with modal

--- GreenPlatform/wwwroot/js/modal.js
+++ GreenPlatform/wwwroot/js/modal.js
@@ -1,35 +1,61 @@
-const closeButton = document.getElementById('close')
-const confirmDeleteButton = document.getElementById('confirmDelete')
-const modal = document.getElementById("close-modal")
-const openButton = document.getElementById("delete-button")
+const openArticles = document.querySelectorAll('[data-article-id]')
 
-const closeModal = () => {
-    modal.classList.remove('my-modal--open')
-    modal.classList.add('my-modal--close')
+openArticles.forEach((article) => {
+    const deleteButton = article.querySelector('[data-article="delete"]')
+    console.log(deleteButton)
+    deleteButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log("qefqefqfeqf")
+        const modalContainer = document.createElement('div')
+        modalContainer.setAttribute('id', 'modal-container')
 
+        document.body.appendChild(modalContainer)
+
+        modalContainer.innerHTML = `
+            <div class="my-modal my-modal--close col" data-modal="modal">
+                <div class="modal-content my-modal__container" data-modal="content">
+                    <div class="modal-body">
+                        <h3>Вы уверены, что хотите удалить статью?</h3>
+                    </div>
+                    <div class="modal-footer">
+                        <form action="/articles/${article.getAttribute('data-article-id')}"
+                              method="post">
+                            <button class="btn btn-danger btn-lg"
+                                    type="submit">
+                                Удалить
+                            </button>
+                        </form>
+                        <button type="button"
+                                class="btn btn-secondary btn-lg"
+                                data-modal="closeModal">
+                                Отмена
+                        </button>
+                    </div>
+                </div>
+            </div>
+            `
+
+        const confirmDelete = modalContainer.querySelectorAll('[data-modal="closeModal"]')
+        const modal = modalContainer.querySelector('[data-modal="modal"]')
+        const modalContent = modalContainer.querySelector('[data-modal="content"]')
+
+        modal.addEventListener('click', (e) => {
+            e.preventDefault()
+            closeModal(modalContainer)
+        })
+
+        modalContent.addEventListener('click', (e) => e.stopPropagation())
+
+        confirmDelete.forEach((el) => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault()
+
+                closeModal(modalContainer)
+            })
+        })
+    })
+})
+
+const closeModal = (element) => {
+    element.remove()
 }
-
-const openModal = () => {
-    modal.classList.add('my-modal--open')
-    modal.classList.remove('my-modal--close')
-}
-
-closeButton.addEventListener('click', () => {
-    closeModal()
-})
-
-confirmDeleteButton.addEventListener('click', () => {
-    closeModal()
-})
-
-modal.addEventListener('click', (e) => {
-    closeModal()
-})
-
-modal.children[0].addEventListener("click", (e) => {
-    e.stopPropagation()
-})
-
-openButton.addEventListener('click', () => {
-    openModal()
-})
